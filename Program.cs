c1918acd8d6a913f867e77ec69fddc2ca8824a94
RoNaLd82QS <ronaldpale@gmail.com>
2025-04-26 02:46:10 -0500

    program

--- Program.cs
+++ Program.cs
@@ -2,20 +2,22 @@ using EquiposPeruanos.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configurar puerto dinámico (usado por Render)
 var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
 builder.WebHost.UseUrls($"http://*:{port}");
 
-
-// Add services
+// Agregar servicios al contenedor
 builder.Services.AddControllersWithViews();
 
+// Configurar DbContext con PostgreSQL
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+    options.UseNpgsql(Environment.GetEnvironmentVariable("DefaultConnection"))
 );
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline
+// Configuración del middleware HTTP
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
@@ -26,12 +28,11 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
 app.UseAuthorization();
 
+// Ruta por defecto: controlador Player, acción Index
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Player}/{action=Index}/{id?}");
-
 
 app.Run();
