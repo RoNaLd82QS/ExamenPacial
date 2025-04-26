f5a0c4470b5b4ed44e6c9263b8027ec61974c2fd
RoNaLd82QS <ronaldpale@gmail.com>
2025-04-26 02:23:45 -0500

    Player

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -4,15 +4,19 @@ using EquiposPeruanos.Models;
 namespace EquiposPeruanos.Data
 {
     public class ApplicationDbContext : DbContext
-    {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options) { }
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) {}
 
-        public DbSet<Player> Players { get; set; }
-        public DbSet<Team> Teams { get; set; }
-        public DbSet<Assignment> Assignments { get; set; }
-        
+    public DbSet<Player> Players { get; set; }
+    public DbSet<Team> Teams { get; set; }
+    public DbSet<Assignment> Assignments { get; set; } // Si tienes la tabla
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        // Aquí puedes configurar relaciones si es necesario
     }
+}
 }
 
 
